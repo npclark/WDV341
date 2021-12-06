75fcce672e1942bedeceb5cd2a1cac014eb2789b
npclark <noah.clark500@gmail.com>
2021-12-06 13:53:12 -0600

    Changed main page to php

--- wdv341.php
+++ wdv341.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
+<html>
 <head>
 	<title>Intro PHP Homework Page</title>
 </head>
 <body>
 	<h1>WDV 341 Intro to PHP</h1>
 	<h2>Homework Links</h2>
-	<ul>
+	<?php echo '
+    <ul>
 		<li><a href="2-1.html">Assignment 2-1</a></li>
-	<ul>
+	<ul>'
+    ?>
 </body>
 </html>
