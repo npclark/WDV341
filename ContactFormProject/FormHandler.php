c41b07c7ed0ec4a0f20653aefe99f77c7611d974
npclark <noah.clark500@gmail.com>
2021-12-07 15:58:18 -0600

    Created Form Project

---
+++ ContactFormProject/FormHandler.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <title>Form Handler</title>
+    <style>
+        body {
+            background-color: lightblue;
+        }
+    </style>
+</head>
+<body>
+
+</body>
+</html>
